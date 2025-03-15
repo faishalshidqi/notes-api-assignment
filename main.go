75328dd9c52c705e726e8b769053552452e37ad8
faishalshidqi <rshidqi6@gmail.com>
2025-03-15 17:05:29 +0700

    added cors config

--- main.go
+++ main.go
@@ -3,6 +3,7 @@ package main
 import (
 	"assignment/commons/bootstrap"
 	"assignment/interfaces/http/api/routes"
+	cors "github.com/gin-contrib/cors"
 	"github.com/gin-gonic/gin"
 	swaggerFiles "github.com/swaggo/files"
 	ginSwagger "github.com/swaggo/gin-swagger"
@@ -32,5 +33,10 @@ func main() {
 	swaggerUrl := ginSwagger.URL("http://localhost:5000/swagger/doc.json")
 	router.GET("/docs/*any", ginSwagger.WrapHandler(swaggerFiles.Handler, swaggerUrl))
 
+	router.Use(cors.New(cors.Config{
+		AllowAllOrigins: true,
+		AllowMethods:    []string{"GET", "POST", "PUT", "PATCH", "DELETE"},
+		AllowHeaders:    []string{"Origin", "Content-Length", "Content-Type", "Authorization"},
+	}))
 	router.Run(env.ServerAddr)
 }
